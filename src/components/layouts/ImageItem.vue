940685a666be6879c90b0cc80efeede60b1570ab
Mateusz Kretowski <matkretkurs@gmail.com>
2023-08-26 17:51:46 +0200

    Add server

---
+++ src/components/layouts/ImageItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <img :src="imageSrc" class="image" />
+</template>
+
+<script>
+import { imagesUrl } from '@/config'
+
+export default {
+  name: 'PhotosList',
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    imageSrc() {
+      return `${imagesUrl}/${this.src}`
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.image {
+  width: 100%;
+}
+</style>
